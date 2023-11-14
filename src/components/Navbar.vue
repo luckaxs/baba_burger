cbcb90f8f68256f95200230343302ac755adf1f5
luckaxs <lucassmariano9@gmail.com>
2023-11-13 21:48:55 -0300

    Projeto iniciado, foi definido as rotas e os componentes iniciais

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Esgulepa</p>
+  </div>
+</template>
